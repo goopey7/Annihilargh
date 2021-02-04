c8710643ccbff077c38f04df2a9790020550b96d
Sam Collier <samcollier916@gmail.com>
2021-02-03 23:26:01 -0500

    Base Anomaly Implementation

---
+++ Annihilargh/BaseAnomaly.cpp
@@ -0,0 +1,38 @@
+﻿#include "BaseAnomaly.h"
+#include <sstream>
+
+BaseAnomaly::BaseAnomaly(int line, const char* file) noexcept
+: line(line),file(file)
+{
+}
+
+const char* BaseAnomaly::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl << GetOriginString();
+	whatBuffer = oss.str(); // whatBuffer persists after function, oss does not.
+	return whatBuffer.c_str();
+}
+
+const char* BaseAnomaly::GetType() const noexcept
+{
+	return "Base Anomaly";
+}
+
+int BaseAnomaly::GetLine() const noexcept
+{
+	return line;
+}
+
+const std::string& BaseAnomaly::GetFile() const noexcept
+{
+	return file;
+}
+
+std::string BaseAnomaly::GetOriginString() const noexcept
+{
+	std::ostringstream oss;
+	oss << "FILE: " << file << std::endl <<
+		"LINE: " << line << std::endl;
+	return oss.str();
+}
