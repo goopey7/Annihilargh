d17595179e2b7065a6418f02f827ee85a64d73fa
Sam Collier <samcollier916@gmail.com>
2021-02-19 11:41:43 -0500

    Added a basic timer class, so we can keep track of change in time between frames

---
+++ Annihilargh/Timer.cpp
@@ -0,0 +1,21 @@
+﻿#include "Timer.h"
+
+#include <chrono>
+
+Timer::Timer() noexcept
+{
+	last=std::chrono::steady_clock::now();
+}
+
+float Timer::Reset() noexcept
+{
+	const auto old = last;
+	last = std::chrono::steady_clock::now();
+	const std::chrono::duration<float> elapsed = last-old;
+	return elapsed.count();
+}
+
+float Timer::GetElapsed() const noexcept
+{
+	return std::chrono::duration<float>(std::chrono::steady_clock::now()-last).count();
+}
---
+++ Annihilargh/Timer.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <chrono>
+class Timer
+{
+public:
+	Timer() noexcept;
+	
+	//returns elapsed time and resets timer
+	float Reset() noexcept;
+
+	//reads elapsed time, doesn't reset timer
+	float GetElapsed() const noexcept;
+private:
+	std::chrono::steady_clock::time_point last;
+	
+};
