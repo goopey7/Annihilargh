5a1dc66dca9c93641a88d568a705ce885e85da03
Sam Collier <samcollier916@gmail.com>
2021-03-21 15:41:27 -0400

    Dynamic Vertex System

--- Annihilargh/Game.cpp
+++ Annihilargh/Game.cpp
@@ -9,10 +9,41 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+#include "VertexSystem.h"
+
 GDIPlusManager gdiPM;
 
-Game::Game(): window(800, 600, "Annihilargh"), light(window.GetGraphics(),0.5f)
+namespace dx = DirectX;
+
+// let's see if this actually compiles and works when stepping through in the debugger. It Does!
+void test()
 {
+	VertexBuffer vertexBuffer(std::move(
+		VertexLayout{}
+		.Append<VertexLayout::Location3D>()
+		.Append<VertexLayout::Normal>()
+		.Append<VertexLayout::TextureCoord2D>()
+	));
+	vertexBuffer.EmplaceBack(
+		dx::XMFLOAT3{1.0f, 2.0f, 3.0f},
+		dx::XMFLOAT3{3.0f, 2.0f, 1.0f},
+		dx::XMFLOAT2{1.0f, 6.0f}
+	);
+	vertexBuffer.EmplaceBack(
+		dx::XMFLOAT3{9.0f, 1.0f, 6.0f},
+		dx::XMFLOAT3{9.0f, 1.0f, 6.0f},
+		dx::XMFLOAT2{9.1f, 6.0f}
+	);
+	auto location = vertexBuffer[0].Retrieve<VertexLayout::Location3D>();
+	auto norm = vertexBuffer[0].Retrieve<VertexLayout::Normal>();
+	auto texture2d = vertexBuffer[1].Retrieve<VertexLayout::TextureCoord2D>();
+	vertexBuffer.Back().Retrieve<VertexLayout::Location3D>().z = 666.0f;
+	location = vertexBuffer.Back().Retrieve<VertexLayout::Location3D>();
+}
+
+Game::Game(): window(800, 600, "Annihilargh"), light(window.GetGraphics(), 0.5f)
+{
+	test();
 	class Factory
 	{
 	public:
@@ -24,10 +55,10 @@ Game::Game(): window(800, 600, "Annihilargh"), light(window.GetGraphics(),0.5f)
 
 		std::unique_ptr<Drawable> operator()()
 		{
-			const DirectX::XMFLOAT3 material = {matDist(rng),matDist(rng),matDist(rng)};
+			const DirectX::XMFLOAT3 material = {matDist(rng), matDist(rng), matDist(rng)};
 			return std::make_unique<IronMan>(
 				gfx, rng, adist, ddist,
-				odist, rdist, material,0.5f
+				odist, rdist, material, 0.5f
 			);
 		}
 
@@ -64,7 +95,7 @@ void Game::Tick()
 	auto deltaTime = timer.Reset() * simulationSpeedFactor;
 	window.GetGraphics().BeginFrame(0.01f, 0.0f, 0.05f);
 	window.GetGraphics().SetCamera(camera.GetMatrix());
-	light.Bind(window.GetGraphics(),camera.GetMatrix());
+	light.Bind(window.GetGraphics(), camera.GetMatrix());
 	for(auto &drawable : drawables)
 	{
 		if(window.keyboard.KeyIsPressed(VK_SPACE)) deltaTime = 0.f;
---
+++ Annihilargh/VertexSystem.h
@@ -0,0 +1,295 @@
+﻿#pragma once
+#include <DirectXMath.h>
+#include <vector>
+
+struct ARGBColour
+{
+	unsigned char a, r, g, b;
+};
+
+// describes how the dumb char buffer in VertexBuffer is structured
+class VertexLayout
+{
+public:
+	enum ElementType
+	{
+		Location2D,
+		Location3D,
+		TextureCoord2D,
+		Normal,
+		Float3Colour,
+		Float4Colour,
+		ARGBColour
+	};
+
+	class Element
+	{
+	public:
+		Element(ElementType type, size_t offset) : type(type), offset(offset)
+		{
+		}
+
+		size_t GetOffsetOfNextElement() const noexcept
+		{
+			return offset + Size();
+		}
+
+		// constexpr so it won't take any runtime resources
+		static constexpr size_t SizeOf(ElementType type) noexcept
+		{
+			switch(type)
+			{
+			case Location2D:
+				return sizeof(DirectX::XMFLOAT2);
+			case Location3D:
+				return sizeof(DirectX::XMFLOAT3);
+			case TextureCoord2D:
+				return sizeof(DirectX::XMFLOAT2);
+			case Normal:
+				return sizeof(DirectX::XMFLOAT3);
+			case Float3Colour:
+				return sizeof(DirectX::XMFLOAT3);
+			case Float4Colour:
+				return sizeof(DirectX::XMFLOAT4);
+			case ARGBColour:
+				return sizeof(unsigned int);
+			}
+			assert("Unknown Element Type" && false);
+			return 0u;
+		}
+
+		size_t Size() const noexcept
+		{
+			return SizeOf(type);
+		}
+
+		ElementType GetType() const noexcept
+		{
+			return type;
+		}
+
+		size_t GetOffset() const noexcept
+		{
+			return offset;
+		}
+	private:
+		ElementType type;
+		size_t offset; // number of bytes away from the beginning of the Vertex struct
+	};
+
+	template <ElementType T>
+	// access element that matches the type. Returns constant reference to it.
+	const Element& Get() const noexcept
+	{
+		for(auto &e : elements)
+		{
+			if(e.GetType() == T)
+			{
+				return e;
+			}
+		}
+		assert("Could not find element type" && false);
+		return elements.back();
+	}
+
+	const Element& Get(size_t index) const noexcept
+	{
+		return elements[index];
+	}
+
+	template <ElementType T>
+	VertexLayout& Append() noexcept
+	{
+		elements.emplace_back(T, Size());
+		return *this;
+	}
+
+	size_t Size() const noexcept
+	{
+		// if vector is empty returns 0. Otherwise returns the size of the struct in bytes
+		// offset after last element is the size of the structure
+		return elements.empty() ? 0u : elements.back().GetOffsetOfNextElement();
+	}
+
+	size_t NumElements() const noexcept
+	{
+		return elements.size();
+	}
+
+private:
+	std::vector<Element> elements;
+};
+
+// represents a vertex within the vertex buffer, and can retrieve attributes
+class Vertex
+{
+	friend class VertexBuffer;
+public:
+	template <VertexLayout::ElementType T>
+	auto& Retrieve() noexcept
+	{
+		namespace dx = DirectX;
+		const auto &element = layout.Get<T>();
+		auto pAttribute = pData + element.GetOffset();
+
+		// I want to use constexpr here so this is determined at compile time
+		// so no switch statement I'm afraid
+		if constexpr(T == VertexLayout::Location2D)
+		{
+			return *reinterpret_cast<dx::XMFLOAT2*>(pAttribute);
+		}
+		else if constexpr(T == VertexLayout::Location3D)
+		{
+			return *reinterpret_cast<dx::XMFLOAT3*>(pAttribute);
+		}
+		else if constexpr(T == VertexLayout::TextureCoord2D)
+		{
+			return *reinterpret_cast<dx::XMFLOAT2*>(pAttribute);
+		}
+		else if constexpr(T == VertexLayout::Normal)
+		{
+			return *reinterpret_cast<dx::XMFLOAT3*>(pAttribute);
+		}
+		else if constexpr(T == VertexLayout::Float3Colour)
+		{
+			return *reinterpret_cast<dx::XMFLOAT3*>(pAttribute);
+		}
+		else if constexpr(T == VertexLayout::Float4Colour)
+		{
+			return *reinterpret_cast<dx::XMFLOAT4*>(pAttribute);
+		}
+		else if constexpr(T == VertexLayout::ARGBColour)
+		{
+			return *reinterpret_cast<ARGBColour*>(pAttribute);
+		}
+		else
+		{
+			assert("Unknown Element Type" && false);
+			return *pAttribute;
+		}
+	}
+
+	template <typename T>
+	// the double && allows us to forward a parameter without loosing any information or modifiers
+	// we then use std::forward to forward it as is. This is using universal references and perfect forwarding
+	void SetAttributeByIndex(size_t i, T &&val) noexcept
+	{
+		namespace dx = DirectX;
+		const auto &element = layout.Get(i);
+		auto pAttribute = pData + element.GetOffset();
+		switch(element.GetType())
+		{
+		case VertexLayout::Location2D:
+			SetAttribute<dx::XMFLOAT2>(pAttribute, std::forward<T>(val));
+			break;
+		case VertexLayout::Location3D:
+			SetAttribute<dx::XMFLOAT3>(pAttribute, std::forward<T>(val));
+			break;
+		case VertexLayout::TextureCoord2D:
+			SetAttribute<dx::XMFLOAT2>(pAttribute, std::forward<T>(val));
+			break;
+		case VertexLayout::Normal:
+			SetAttribute<dx::XMFLOAT3>(pAttribute, std::forward<T>(val));
+			break;
+		case VertexLayout::Float3Colour:
+			SetAttribute<dx::XMFLOAT3>(pAttribute, std::forward<T>(val));
+			break;
+		case VertexLayout::Float4Colour:
+			SetAttribute<dx::XMFLOAT4>(pAttribute, std::forward<T>(val));
+			break;
+		case VertexLayout::ARGBColour:
+			SetAttribute<ARGBColour>(pAttribute, std::forward<T>(val));
+			break;
+		default:
+			assert("Unknown Element Type" && false);
+		}
+	}
+
+private:
+	Vertex(char* pData, const VertexLayout &layout) noexcept : pData(pData), layout(layout)
+	{
+		assert(pData != nullptr);
+	}
+
+	// We don't know how many parameters we're going to get
+	// so the ... represents a 'parameter pack' meaning it can represent any number of parameters
+	// you can't iterate on this pack, so we have to do recursion.
+	// A quick explanation is that this function will peel off the first parameter from the pack, and process it.
+	// Then we do recursion and keep doing that until we have processed every parameter.
+	template<typename First, typename ...Rest>
+	void SetAttributeByIndex(size_t i, First &&first,Rest &&...rest) noexcept
+	{
+		SetAttributeByIndex(i,std::forward<First>(first)); // evaluates to the above public SetAttributeByIndex()
+		SetAttributeByIndex(i+1,std::forward<Rest>(rest)...); // recurse, peels back first, recurses again.
+		// once rest contains only one parameter, this call will resolve to the above SetAttributeByIndex()
+		// so that's how the recursion will end.
+	}
+
+	// this function checks to see if the type we are attempting to cast to will actually work.
+	// it's called in the public SetAttributByIndex()
+	template <typename Dest, typename Src>
+	void SetAttribute(char* pAttribute, Src &&val) noexcept
+	{
+		if constexpr(std::is_assignable<Dest, Src>::value)
+		{
+			*reinterpret_cast<Dest*>(pAttribute) = val;
+		}
+		else
+			assert("Fail to cast attribute type" && false);
+	}
+
+private:
+	char* pData = nullptr;
+	const VertexLayout &layout;
+};
+
+// represents the data
+class VertexBuffer
+{
+public:
+	VertexBuffer(VertexLayout layout) noexcept : layout(std::move(layout))
+	{
+	}
+
+	const VertexLayout& GetLayout() const noexcept
+	{
+		return layout;
+	}
+
+	size_t Size() const noexcept // Gets the number of vertices, not the size in bytes
+	{
+		return buffer.size() / layout.Size();
+	}
+
+	Vertex Front() noexcept
+	{
+		assert(buffer.size()>=0u);
+		return Vertex{buffer.data(), layout};
+	}
+
+	Vertex Back() noexcept
+	{
+		assert(buffer.size()>=0u);
+		return Vertex{buffer.data() + buffer.size() - layout.Size(), layout};
+	}
+
+	Vertex operator[](size_t i) noexcept
+	{
+		assert(i < Size());
+		return Vertex{buffer.data() + layout.Size() * i, layout};
+	}
+
+	// takes in a parameter pack and processes them. Refer to the private field of Vertex for more info
+	template <typename ...Params>
+	void EmplaceBack(Params &&... params) noexcept
+	{
+		// the number of parameters should match the amount of elements in the layout
+		assert(sizeof...(params) == layout.NumElements() && "Number of params doesn't match number of elements in layout");
+		buffer.resize(buffer.size() + layout.Size());
+		Back().SetAttributeByIndex(0u,std::forward<Params>(params)...);
+	}
+
+private:
+	std::vector<char> buffer; // unstructured buffer of bytes
+	VertexLayout layout; // layout describing the structure
+};
